94f452ca18907beee4c8494427409c74ac7fc967
Mariam Slaiei <10120163@mu.edu.lb>
2025-03-31 23:08:49 +0300

    set up the project

---
+++ src/main/java/com/capstone/demo/CarepointApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarepointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarepointApplication.class, args);
+	}
+
+}
